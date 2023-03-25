18d20d48e8f87be3250f2db4efbe0bfc6f76fb88
Ezra Shaw <ezrasure@outlook.com>
2023-03-25 13:39:37 +1300

    perf: improve perf by taking stdout lock once

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![feature(iter_array_chunks)]
 
-use std::env;
+use std::{
+    env,
+    io::{self, Write},
+    io::{stdout, StdoutLock},
+};
 
 use image::{imageops::FilterType, io::Reader, DynamicImage};
 use termsize::Size;
@@ -22,20 +26,25 @@ fn main() {
         FilterType::Triangle,
     );
 
-    print_image(img);
+    print_image(img).unwrap();
 }
 
-fn print_image(img: DynamicImage) {
+fn print_image(img: DynamicImage) -> io::Result<()> {
+    let mut handle = stdout().lock();
+
     for [top_row, btm_row] in img.into_rgb8().rows().array_chunks::<2>() {
         for (&top, &btm) in top_row.zip(btm_row) {
-            print_cell(top.0, btm.0);
+            print_cell(&mut handle, top.0, btm.0)?;
         }
-        println!("\x1B[0m");
+        writeln!(handle, "\x1B[0m")?;
     }
+
+    Ok(())
 }
 
-fn print_cell(top: [u8; 3], btm: [u8; 3]) {
-    print!("\x1B[38;2;{};{};{}m", top[0], top[1], top[2]);
-    print!("\x1B[48;2;{};{};{}m", btm[0], btm[1], btm[2]);
-    print!("▀");
+fn print_cell(handle: &mut StdoutLock, top: [u8; 3], btm: [u8; 3]) -> io::Result<()> {
+    write!(handle, "\x1B[38;2;{};{};{}m", top[0], top[1], top[2])?;
+    write!(handle, "\x1B[48;2;{};{};{}m", btm[0], btm[1], btm[2])?;
+
+    write!(handle, "▀")
 }
